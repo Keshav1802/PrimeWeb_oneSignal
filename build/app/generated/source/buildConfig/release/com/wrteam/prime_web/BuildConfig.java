39e17005bf391faa5e4865be3851457f36ce2758
Keshav Kumar <keshavkumar8738@gmail.com>
2023-02-12 00:47:56 +0530

    2nd major

---
+++ build/app/generated/source/buildConfig/release/com/wrteam/prime_web/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.wrteam.prime_web;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.mothering.leads";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.0.2";
+}
